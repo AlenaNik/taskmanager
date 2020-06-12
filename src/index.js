5a8ef7880fb1baa6fd54536db01370d4decaf867
AlenaNik <alena.nik.digital@gmail.com>
2020-06-12 15:42:44 +0200

    plugin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import defaultBoard from './default-board'
+import { saveStatePlugin } from './utils'
 
 Vue.use(Vuex)
 
 const board = JSON.parse(localStorage.getItem('board')) || defaultBoard
 
 export const store = new Vuex.Store({
+  plugins: [saveStatePlugin],
   state: {
     board
   },
